68549d8f0cb61506dbb8ce6624f19587836ab0ce
laseredface <laseredface@gmail.com>
2019-01-29 20:37:09 -0700

    Initial Real Code

---
+++ src/main/java/frc/robot/HelloJNI.java
@@ -0,0 +1,19 @@
+
+package frc.robot;
+
+public class HelloJNI {  // Save as HelloJNI.java
+    static {
+       System.loadLibrary("hello"); // Load native library hello.dll (Windows) or libhello.so (Unixes)
+                                    //  at runtime
+                                    // This library contains a native method called sayHello()
+    }
+  
+    // Declare an instance native method sayHello() which receives no parameter and returns void
+    private native void sayHello();
+  
+    // Test Driver
+    public static void main(String[] args) {
+      System.out.println("Hello from java!") ;
+      new HelloJNI().sayHello();  // Create an instance and invoke the native method
+    }
+ }
